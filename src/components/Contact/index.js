38f5fd3c110f6823a4ffe03c56f31626d67d73d3
Bonny Boben <bonnybobencr7@gmail.com>
2023-08-13 22:05:32 +0530

    contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,18 +1,50 @@
 import React from "react";
 import "./index.css";
+import tick from "../../assets/tick.png";
+import Reveal from "../utils/Reveal";
 
 const Contact = () => {
   return (
     <div className="contact-container">
       <div className="contact-sub-container">
-        <h1 className="contact-heading">1% OF THE INDUSTRY</h1>
-        <h1 className="contact-sub-heading">
-          Welcome to your new digital reality that will rock your world truly at
-          all throughout.
-        </h1>
-        <div>
-          <input type="text" />
-          <button>Submit</button>
+        <Reveal>
+          <h1 className="contact-heading">1% OF THE INDUSTRY</h1>
+        </Reveal>
+        <Reveal>
+          <h1 className="contact-sub-heading">
+            Welcome to your new digital reality that will rock your world truly
+            at all throughout.
+          </h1>
+        </Reveal>
+        <Reveal>
+          <div className="contact-box">
+            <input
+              className="contact-input"
+              type="text"
+              placeholder="Enter Your Email"
+            />
+            <button className="submit-button">Submit</button>
+          </div>
+        </Reveal>
+        <div className="contact-feature-container">
+          <Reveal>
+            <div className="contact-feature-box">
+              <img className="contact-feature-logo" src={tick} />
+              <p className="contact-feature-heading">Fully Secure</p>
+            </div>
+          </Reveal>
+          <Reveal>
+            <div className="contact-feature-box">
+              <img className="contact-feature-logo" src={tick} />
+              <p className="contact-feature-heading">24/7 Support</p>
+            </div>
+          </Reveal>
+          <Reveal>
+            <div className="contact-feature-box">
+              <img className="contact-feature-logo" src={tick} />
+              <p className="contact-feature-heading">Done Deal</p>
+            </div>
+          </Reveal>
         </div>
       </div>
     </div>
